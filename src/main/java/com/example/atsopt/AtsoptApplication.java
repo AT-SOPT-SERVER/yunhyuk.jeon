f01f9527edc8cafb710468f3c82bb47bebe16d9f
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-04-12 16:47:35 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/atsopt/AtsoptApplication.java
@@ -0,0 +1,13 @@
+package com.example.atsopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsoptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atsopt/AtsoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atsopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
